57f3d6166494df2acd7c6dcecb9043ca20957434
Alessio Saltarin <alessiosaltarin@gmail.com>
2021-01-11 10:17:37 +0100

    Added Swagger

--- smartdjangorest/urls.py
+++ smartdjangorest/urls.py
@@ -6,8 +6,12 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
+
 from smartdjangorest import views
+
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -19,6 +23,15 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('openapi-schema', get_schema_view(
+            title="SmartDjango REST",
+            description="API for SmartDjango",
+            version="1.0.0"
+        ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger_ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
